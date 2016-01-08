7554f16d4f2d8e41711194228cdbecedc7c087c2
jsRuner <hi_php@163.com>
2016-01-09 00:21:08 +0800

    添加了对外的接口

--- auto2writenote.py
+++ auto2writenote.py
@@ -2,6 +2,9 @@
 # encoding: utf-8
 
 import time
+import urllib
+import urllib2
+import json
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
@@ -17,12 +20,17 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 @file: auto2writenote.py
 @time: 2016/1/8 13:32
 自动填写笔记。
-
 实现自动读取多个账号。无则新建，有则编辑。
+需要从数据库读取账号。读取要填写的日志。
+账号表：id email password addtime updatetime 配置参数（何时发布日志，是否接受邮件通知，邮件地址），是否通过审核，
+日志内容表：关联的账号id,日志内容。
 
+操作的日志记录：id addtime msg
 
+python不操作数据库。通过发送htt请求来实现。
 
-
+接口：获取账号列表。
+接口:发送日志记录。
 
 """
 
@@ -42,7 +50,7 @@ def selenium(url,d,username,password,content):
     else:
         driver = d
     driver.get(url)
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(10)
     # 填写表单
     driver.find_element_by_id("email").send_keys(username)
     driver.find_element_by_id("password").send_keys(password)
@@ -50,6 +58,15 @@ def selenium(url,d,username,password,content):
     time.sleep(5)
     # 登录
     driver.find_element_by_css_selector(".btn-submit").click()
+
+    #这里要进行判断是否登录成功。如果不成功。则关闭浏览器。
+    time.sleep(10)
+    currenturl = driver.execute_script("return window.location.href;")
+    if currenturl == url:
+        print(u"登录失败")
+        driver.close()
+        return ''
+
     print(u'登录了')
     #强制睡眠。
     time.sleep(10)
@@ -67,23 +84,34 @@ def selenium(url,d,username,password,content):
 
         # 执行点击新建工作日志。第一次填写。table.table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > a:nth-child(1)
         driver.execute_script('$("table.table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > a:nth-child(1)").click()')
+        # 等待几秒后去填写日记
+        time.sleep(5)
+        # 这里容易出异常
+        driver.execute_script("CKEDITOR.instances['modalContent'].setData('%s')" % content)
+        print(u'日记填写完毕:%s' % content)
+        time.sleep(5)
+        # 点击提交。
+        driver.find_element_by_id('btn_worklog_ok').click()
+
     else:
-        print(u"本次操作属于编辑工作日记")
-        # 说明已经写过了。这里应该是进行一次编辑即可。
-        driver.execute_script('$(".table > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(3) > a:nth-child(1) > i:nth-child(1)").click()')
-    # 等待几秒后去填写日记
+        print(u"本次操作属于编辑工作日记,开始退出浏览器")
     time.sleep(5)
-    driver.execute_script("CKEDITOR.instances['modalContent'].setData('%s')" % content)
-    print(u'日记填写完毕:%s' % content)
-    time.sleep(5)
-    # 点击提交。
-    driver.find_element_by_id('btn_worklog_ok').click()
     #关闭浏览器
     driver.close()
     print(u'退出成功')
+    return ''
+
+def get_info():
+    f=urllib.urlopen("http://wuwenfu.cn/?get_jin_post=luoding123")
+
+    s=f.read()
+    d = json.loads(s)
+
+    return d
 
 
 
+    pass
 
 class Main():
     def __init__(self):
@@ -92,15 +120,26 @@ class Main():
 
 if __name__ == '__main__':
     website = 'https://sso.jingoal.com/oauth/authorize?client_id=jmbmgtweb&response_type=code&state={access_count%3A1}&redirect_uri=http%3A%2F%2Fweb.jingoal.com%2Fmgt2%2F%3Flocale%3Dzh_CN#/login'
-    notecontent = u"继续填写日记"
 
-    selenium(website,None,'540045865@qq.com','luoding123',notecontent)
+    # 测试是否能正常运行。挂机一夜。
+    requestsecond = 10
+    # 10秒执行一次。请求php。请求获取账号信息。向博客发送请求api。
+    count = 0
 
-    selenium(website,None,'18256963312','htt199002',notecontent)
+    while True:
+        l = get_info()
+        for item in l:
+            print(item['email'])
+            print(item['password'])
+            print(item['msg'])
+            count = count + 1
 
-    print(u'结束了')
+            start = time.time()
+            selenium(website,None,item['email'],item['password'],item['msg'])
+            spend = time.time() - start
+
+            print('执行第%d次,花费时间:%d'% (count,spend))
+
+        time.sleep(requestsecond)
 
 
-
-
-    pass
